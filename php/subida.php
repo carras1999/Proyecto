b2adf674a13fd1098a5f535f3ef60f9e7c179eb3
AS3-2-05 <AS3-2-05@uni.lan>
2019-01-24 13:46:16 +0100

    cambios

--- php/subida.php
+++ php/subida.php
@@ -53,46 +53,7 @@ body {
 
 
 <body id="index" class="split">
-<?php
-    # definimos la carpeta destino
-    $carpetaDestino="imagenes/";
- 
-    # si hay algun archivo que subir
-    if(isset($_FILES["archivo"]) && $_FILES["archivo"]["name"][0])
-    {
- 
-        # recorremos todos los arhivos que se han subido
-        for($i=0;$i<count($_FILES["archivo"]["name"]);$i++)
-        {
- 
-            # si es un formato de imagen
-            if($_FILES["archivo"]["type"][$i]=="image/jpeg" || $_FILES["archivo"]["type"][$i]=="image/pjpeg" || $_FILES["archivo"]["type"][$i]=="image/gif" || $_FILES["archivo"]["type"][$i]=="image/png")
-            {
- 
-                # si exsite la carpeta o se ha creado
-                if(file_exists($carpetaDestino) || @mkdir($carpetaDestino))
-                {
-                    $origen=$_FILES["archivo"]["tmp_name"][$i];
-                    $destino=$carpetaDestino.$_FILES["archivo"]["name"][$i];
- 
-                    # movemos el archivo
-                    if(@move_uploaded_file($origen, $destino))
-                    {
-                        echo "<br>".$_FILES["archivo"]["name"][$i]." movido correctamente";
-                    }else{
-                        echo "<br>No se ha podido mover el archivo: ".$_FILES["archivo"]["name"][$i];
-                    }
-                }else{
-                    echo "<br>No se ha podido crear la carpeta: ".$carpetaDestino;
-                }
-            }else{
-                echo "<br>".$_FILES["archivo"]["name"][$i]." - NO es imagen jpg, png o gif";
-            }
-        }
-    }else{
-        
-    }
-    ?>
+
 <div id="home-cover" data-content="follow-scroll-opacity">
 
 	<div id="home-cover-content" class="c20 fluid-column center-box padding-left-10 padding-right-10">
