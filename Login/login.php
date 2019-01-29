899c5d4c6ba3a2a558ff26c05c19a2ad07133129
beviga1999 <as3-2-21.18@uni.eus>
2019-01-29 09:04:17 +0100

    .

--- Login/login.php
+++ Login/login.php
@@ -11,14 +11,13 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "web";
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
     die("Connection failed: ". mysqli_connect_error());
 }
 else{
-//echo "Connected succesfully"."<br>";}
+echo "Connected succesfully"."<br>";}
 
 //select from user where
 $query = "SELECT pass FROM usuarios WHERE user = '$user'";
@@ -26,9 +25,6 @@ $result = mysqli_query($conn, $query);
 //var_dump($result);
 $row = mysqli_fetch_array($result, MYSQLI_NUM);
 $hash = $row[0];
-
-/*$hash = password_hash($passwd, PASSWORD_DEFAULT);*/
-
 if (password_verify($passwd, $hash)){
     //sesion
     session_start();
@@ -37,6 +33,8 @@ if (password_verify($passwd, $hash)){
     header("Location: ../index.html");
 
 } else {
-    
+
     $error=1;
-    echo $twig->render('login.html', ['error' => $error] );}
+    echo $twig->render('login.html', ['error' => $error]);
+
+}
